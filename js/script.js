8f555bfeacab62b90b54c789be378188174d3699
antoniovenezia <antoniovenezia00@gmail.com>
2021-06-15 16:27:10 +0200

    js-oggetti-studenti

---
+++ js/script.js
@@ -0,0 +1,57 @@
+//PARTE 1
+
+//Creare un oggetto che descriva uno studente, con le seguenti proprietà: nome, cognome e età. 
+//Stampare a schermo attraverso un ciclo for-in tutte le proprietà dell’oggetto.
+
+var studentElement = document.getElementById("part-1");
+
+var studentPart1 = {
+    name: "Antonio",
+    surname: "Venezia",
+    age: "21"
+}
+
+for (var k in studentPart1) {
+    studentElement.innerHTML += k + ": " + studentPart1[k] + "<br>";
+}
+
+
+//PARTE 2
+
+//Creare un array di oggetti di studenti. Ciclare su tutti gli studenti e stampare per ognuno 
+//di essi, nome e cognome.  Ricordatevi, è un array, quindi? for.
+
+var studentElement2 = document.getElementById("part-2");
+
+var studentPart2 = {
+    name: ["Antonio", "Andrea", "Gianni", "Roberto", "Luca"],
+    surname: ["Venezia", "Ditaranto", "Sulla", "Armandi", "Appio"],
+}
+
+for (var x = 0; x < studentPart2.length; x++) {
+    
+}
+
+
+//PARTE 3
+
+//Dare la possibilità all’utente, attraverso 3 prompt() - uno per ciascuna proprietà - di 
+//aggiungere UN SOLO oggetto studente inserendo nell’ordine: nome, cognome e età.
+
+var classPart3 = {
+    studentsPart3: []
+}
+
+while (classPart3.studentsPart3.length < 1) {
+    var studentName3 = prompt("Inserisci il nome dello studente");
+    var studentSurname3 = prompt("Inserisci il cognome dello studente");
+    var studentAge3 = prompt("Inserisci l'età dello studente");
+    
+    classPart3.studentsPart3.push({
+        name3: studentName3,
+        surname3: studentSurname3,
+        age3: studentAge3,
+    });
+}
+
+console.log(classPart3);
